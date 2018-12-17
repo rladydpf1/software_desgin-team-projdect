313bee2435b2005d9a8f67b7cd0229a606984b3c
rladydpf1 <45200209+rladydpf1@users.noreply.github.com>
2018-12-17 21:01:55 +0900

    Update Shopping_mall.sql

--- Shopping_mall.sql
+++ Shopping_mall.sql
@@ -1,3 +1,10 @@
+DROP TABLE SHOPPINGBASKET;
+DROP TABLE MANAGER;
+DROP TABLE CUSTOMER;
+DROP TABLE STOCK;
+DROP TABLE PERSON;
+DROP TABLE REQUEST;
+
 CREATE TABLE PERSON (
 ID 		varchar(20) not null, 
 Last_name 	varchar(20),
@@ -27,9 +34,8 @@ FOREIGN KEY(M_ID) REFERENCES PERSON (ID)
 
 CREATE TABLE REQUEST (
 Rnumber 	int not null,
-Ritem		varchar(20) not null,
+Ino		int not null,
 Rdate		DATE,
-destination	int,
 PRIMARY KEY(Rnumber)
 );
 
@@ -49,3 +55,14 @@ FOREIGN KEY(Inum) REFERENCES STOCK (Inumber),
 FOREIGN KEY(Cnum) REFERENCES CUSTOMER (Cnumber) 
 );
 
+INSERT INTO PERSON VALUES ('manager1', 'Kim', 'Yoel', 'Kyunpook University', '010-2305-7167',  STR_TO_DATE('1999-02-02' ,'%Y-%m-%d'), 22, 'manager2');
+INSERT INTO PERSON VALUES ('customer1', 'Lee', 'Hogyung', 'Kyunpook University', '010-1234-5678',  STR_TO_DATE('1950-12-18' ,'%Y-%m-%d'), 60, 'customer2');
+INSERT INTO CUSTOMER VALUES (1, 'customer1');
+INSERT INTO MANAGER VALUES (1, 'manager1', 'Sales');
+INSERT INTO REQUEST VALUES (1, 1, STR_TO_DATE('2018-12-18' ,'%Y-%m-%d'));
+INSERT INTO STOCK VALUES (1, 'banana', 10);
+INSERT INTO STOCK VALUES (2, 'apple', 20);
+INSERT INTO STOCK VALUES (3, 'coat', 1);
+INSERT INTO STOCK VALUES (4, 'dress', 0);
+INSERT INTO SHOPPINGBASKET VALUES (1, 1, 4);
+INSERT INTO SHOPPINGBASKET VALUES (1, 3, 2);
